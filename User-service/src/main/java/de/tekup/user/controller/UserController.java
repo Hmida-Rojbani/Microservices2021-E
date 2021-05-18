8424cdd760b0ffc05371a678baa67b0ff035b534
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-05-18 10:18:13 +0100

    multi instance

--- User-service/src/main/java/de/tekup/user/controller/UserController.java
+++ User-service/src/main/java/de/tekup/user/controller/UserController.java
@@ -1,5 +1,7 @@
 package de.tekup.user.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+	@Autowired
+	private Environment env;
 	
 	@GetMapping
 	public String works() {
-		return "User MS works";
+		return "User MS works on port :"+ env.getProperty("local.server.port");
 	}
 
 }
