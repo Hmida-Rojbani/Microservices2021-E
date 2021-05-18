8b529988dcf895b84999ec26efd2f3c3d2c06b64
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-05-18 09:27:00 +0100

    3 MS

---
+++ Discovery-service/src/test/java/de/tekup/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tekup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
